89f0c436879f0fb7bbd2446037c59105645ff5f8
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-01-12 04:09:43 +0100

    Update HologramTeleportCommand

--- src/main/java/toutouchien/niveriaholograms/commands/hologram/basic/HologramTeleportCommand.java
+++ src/main/java/toutouchien/niveriaholograms/commands/hologram/basic/HologramTeleportCommand.java
@@ -1,5 +1,6 @@
 package toutouchien.niveriaholograms.commands.hologram.basic;
 
+/*
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.entity.Player;
@@ -60,5 +61,55 @@ public class HologramTeleportCommand extends SubCommand {
 				.map(Hologram::name)
 				.filter(hologramName -> hologramName.toLowerCase(Locale.ROOT).startsWith(currentArg))
 				.toList();
+	}
+}
+*/
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import toutouchien.niveriaapi.lang.Lang;
+import toutouchien.niveriaapi.utils.CommandUtils;
+import toutouchien.niveriaholograms.NiveriaHolograms;
+import toutouchien.niveriaholograms.core.Hologram;
+import toutouchien.niveriaholograms.managers.HologramManager;
+
+public class HologramTeleportCommand {
+	private HologramTeleportCommand() {
+		throw new IllegalStateException("Command class");
+	}
+
+	public static LiteralCommandNode<CommandSourceStack> get() {
+		return Commands.literal("teleport")
+				.requires(css -> CommandUtils.defaultRequirements(css, "niveriaholograms.command.hologram.teleport", true))
+				.then(Commands.argument("hologram", StringArgumentType.word())
+						.suggests((ctx, builder) -> {
+							HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
+
+							for (Hologram hologram : hologramManager.holograms())
+								builder.suggest(hologram.name());
+
+							return builder.buildFuture();
+						})
+						.executes(ctx -> {
+							Player player = (Player) ctx.getSource().getExecutor();
+							String hologramName = StringArgumentType.getString(ctx, "hologram");
+
+							HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
+							Hologram hologram = hologramManager.hologramByName(hologramName);
+							if (hologram == null) {
+								Lang.sendMessage(player, "niveriaholograms.hologram.teleport.doesnt_exist", hologramName);
+								return Command.SINGLE_SUCCESS;
+							}
+
+							player.teleportAsync(hologram.location().bukkitLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+							Lang.sendMessage(player, "niveriaholograms.hologram.teleport.teleported", hologram.name());
+							return Command.SINGLE_SUCCESS;
+						})
+				).build();
 	}
 }
