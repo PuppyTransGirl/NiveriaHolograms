d605e91e7219fb577f28d93d6f1658689e44e27b
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-01-12 03:10:39 +0100

    Update HologramInfoCommand

--- src/main/java/toutouchien/niveriaholograms/commands/hologram/basic/HologramInfoCommand.java
+++ src/main/java/toutouchien/niveriaholograms/commands/hologram/basic/HologramInfoCommand.java
@@ -1,155 +1,136 @@
 package toutouchien.niveriaholograms.commands.hologram.basic;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.NamedTextColor;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.format.TextColor;
+import net.minecraft.util.Brightness;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
-import toutouchien.niveriaapi.command.CommandData;
-import toutouchien.niveriaapi.command.SubCommand;
-import toutouchien.niveriaapi.utils.common.MathUtils;
-import toutouchien.niveriaapi.utils.ui.ColorUtils;
-import toutouchien.niveriaapi.utils.ui.MessageUtils;
+import toutouchien.niveriaapi.lang.Lang;
+import toutouchien.niveriaapi.utils.CommandUtils;
+import toutouchien.niveriaapi.utils.StringUtils;
 import toutouchien.niveriaholograms.NiveriaHolograms;
 import toutouchien.niveriaholograms.configurations.BlockHologramConfiguration;
 import toutouchien.niveriaholograms.configurations.HologramConfiguration;
 import toutouchien.niveriaholograms.configurations.ItemHologramConfiguration;
 import toutouchien.niveriaholograms.configurations.TextHologramConfiguration;
 import toutouchien.niveriaholograms.core.Hologram;
+import toutouchien.niveriaholograms.core.HologramType;
 import toutouchien.niveriaholograms.managers.HologramManager;
 import toutouchien.niveriaholograms.utils.CustomLocation;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 
-import static net.kyori.adventure.text.Component.text;
-
-public class HologramInfoCommand extends SubCommand {
-    HologramInfoCommand() {
-        super(new CommandData("info", "niveriaholograms")
-                .aliases("i")
-                .usage("<hologram>"));
+public class HologramInfoCommand {
+    private HologramInfoCommand() {
+        throw new IllegalStateException("Command class");
     }
 
-    @Override
-    public void execute(@NotNull CommandSender sender, String[] args, @NotNull String label) {
-        if (args.length == 0) {
-            TextComponent errorMessage = MessageUtils.errorMessage(
-                    text("Tu dois spécifier le nom de l'hologramme auquel tu veux te téléporter.")
-            );
+    public static LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("info")
+                .requires(css -> CommandUtils.defaultRequirements(css, "niveriaholograms.command.hologram.info"))
+                .then(Commands.argument("hologram", StringArgumentType.word())
+                        .suggests((ctx, builder) -> {
+                            HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
 
-            sender.sendMessage(errorMessage);
-            return;
-        }
+                            for (Hologram hologram : hologramManager.holograms())
+                                builder.suggest(hologram.name());
 
-        HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
-        Hologram hologram = hologramManager.hologramByName(args[0]);
-        if (hologram == null) {
-            TextComponent errorMessage = MessageUtils.errorMessage(
-                    text("Cet hologramme n'existe pas.")
-            );
+                            return builder.buildFuture();
+                        })
+                        .executes(ctx -> {
+                            CommandSender sender = CommandUtils.sender(ctx);
+                            String hologramName = ctx.getArgument("hologram", String.class);
 
-            sender.sendMessage(errorMessage);
-            return;
-        }
+                            HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
+                            Hologram hologram = hologramManager.hologramByName(hologramName);
+                            if (hologram == null) {
+                                Lang.sendMessage(sender, "niveriaholograms.hologram.info.doesnt_exist", hologramName);
+                                return Command.SINGLE_SUCCESS;
+                            }
 
-        HologramConfiguration config = hologram.configuration();
+                            HologramConfiguration config = hologram.configuration();
+                            HologramType type = hologram.type();
+                            CustomLocation loc = hologram.location();
 
-        CustomLocation location = hologram.location();
-        Component infoMessage = text("Informations sur l'hologramme " + hologram.name() + ":", ColorUtils.primaryColor())
-                .appendNewline()
-                .append(text("Nom: ", ColorUtils.secondaryColor()))
-                .append(text(hologram.name(), NamedTextColor.WHITE))
-                .appendNewline()
-                .append(text("Type: ", ColorUtils.secondaryColor()))
-                .append(text(hologram.type().name(), NamedTextColor.WHITE))
-                .appendNewline()
-                .append(text("Position: ", ColorUtils.secondaryColor()))
-                .append(text(
-                        MathUtils.decimalRound(location.x(), 2) + "/"
-                                + MathUtils.decimalRound(location.y(), 2) + "/"
-                                + MathUtils.decimalRound(location.z(), 2)
-                                + " in " + location.world(), NamedTextColor.WHITE
-                ))
-                .appendNewline()
-                .append(text("Rotation: ", ColorUtils.secondaryColor()))
-                .append(text(
-                        MathUtils.decimalRound(location.yaw(), 2) + "°/"
-                                + MathUtils.decimalRound(location.pitch(), 2) + "°", NamedTextColor.WHITE
-                ))
-                .appendNewline()
-                .append(text("Taille: ", ColorUtils.secondaryColor()))
-                .append(text(scaleText(config.scale()), NamedTextColor.WHITE))
-                .appendNewline()
-                .append(text("Billboard: ", ColorUtils.secondaryColor()))
-                .append(text(config.billboard().name(), NamedTextColor.WHITE))
-                .appendNewline()
-                .append(text("Rayon d'ombre: ", ColorUtils.secondaryColor()))
-                .append(text(config.shadowRadius(), NamedTextColor.WHITE))
-                .appendNewline()
-                .append(text("Force de l'ombre: ", ColorUtils.secondaryColor()))
-                .append(text(config.shadowStrength(), NamedTextColor.WHITE))
-                .appendNewline();
+                            Lang.sendMessage(sender, "niveriaholograms.hologram.info.info",
+                                    hologram.name(),
+                                    StringUtils.capitalize(type.name()),
+                                    loc.x(), loc.y(), loc.z(),
+                                    hologram.location().world(),
+                                    loc.yaw(), loc.pitch(),
+                                    scaleText(config.scale()),
+                                    StringUtils.capitalize(config.billboard().name()),
+                                    brightnessText(config.brightness()),
+                                    config.shadowRadius(),
+                                    config.shadowStrength(),
+                                    config.visibilityDistance()
+                            );
 
-        switch (config) {
-            case BlockHologramConfiguration blockConfig ->
-                    infoMessage = infoMessage.append(text("Bloc: ", ColorUtils.secondaryColor()))
-                            .append(text(blockConfig.material().name(), NamedTextColor.WHITE));
+                            String noGlowingColorText = Lang.getString("niveriaholograms.hologram.other.no_glowing_color");
+                            switch (config) {
+                                case BlockHologramConfiguration block ->
+                                        Lang.sendMessage(sender, "niveriaholograms.hologram.info.info_block",
+                                                block.material().name(),
+                                                block.glowing(),
+                                                block.glowingColor() == null ? noGlowingColorText : block.glowingColor().asHexString()
+                                        );
 
-            case ItemHologramConfiguration itemConfig ->
-                    infoMessage = infoMessage.append(text("Item: ", ColorUtils.secondaryColor()))
-                            .append(text(itemConfig.itemStack().getType().name(), NamedTextColor.WHITE));
+                                case ItemHologramConfiguration item ->
+                                        Lang.sendMessage(sender, "niveriaholograms.hologram.info.info_item",
+                                                item.itemStack().getType(),
+                                                item.glowing(),
+                                                item.glowingColor() == null ? noGlowingColorText : item.glowingColor().asHexString()
+                                        );
 
-            case TextHologramConfiguration textConfig -> {
-                for (String line : textConfig.text()) {
-                    infoMessage = infoMessage.append(text(" - " + line, NamedTextColor.WHITE))
-                            .appendNewline();
-                }
+                                case TextHologramConfiguration text ->
+                                        Lang.sendMessage(sender, "niveriaholograms.hologram.info.info_text",
+                                                backgroundText(text.background()),
+                                                text.textAlignment().name(),
+                                                text.seeThrough(),
+                                                text.shadowRadius(),
+                                                text.updateInterval()
+                                        );
 
-                infoMessage = infoMessage.append(text("Fond: ", ColorUtils.secondaryColor()))
-                        .append(text(backgroundText(textConfig.background()), NamedTextColor.WHITE))
-                        .appendNewline()
-                        .append(text("Alignement du texte: ", ColorUtils.secondaryColor()))
-                        .append(text(textConfig.textAlignment().name(), NamedTextColor.WHITE))
-                        .appendNewline()
-                        .append(text("Transparent: ", ColorUtils.secondaryColor()))
-                        .append(text(textConfig.seeThrough() ? "Oui" : "Non", NamedTextColor.WHITE))
-                        .appendNewline()
-                        .append(text("Ombre du texte: ", ColorUtils.secondaryColor()))
-                        .append(text(textConfig.textShadow() ? "Oui" : "Non", NamedTextColor.WHITE));
-            }
+                                default -> throw new IllegalStateException("Unexpected value: " + config);
+                            }
 
-            default -> infoMessage = infoMessage.append(text("Configuration inconnue.", NamedTextColor.WHITE));
-        }
-
-        sender.sendMessage(infoMessage);
+                            return Command.SINGLE_SUCCESS;
+                        })
+                ).build();
     }
 
     @NotNull
-    private String scaleText(@NotNull Vector3f scale) {
+    private static String scaleText(@NotNull Vector3f scale) {
         boolean allEquals = scale.x() == scale.y() && scale.y() == scale.z();
         return allEquals ? Float.toString(scale.x()) : scale.x() + " " + scale.y() + " " + scale.z();
     }
 
     @NotNull
-    private String backgroundText(@Nullable TextColor background) {
-        return background == null ? "Par défaut" : background == Hologram.TRANSPARENT ? "Transparent" : background.asHexString().toUpperCase(Locale.ROOT);
+    private static String[] brightnessText(@Nullable Brightness brightness) {
+        String defaultText = Lang.getString("niveriaholograms.hologram.info.other.default");
+
+        if (brightness == null)
+            return new String[]{defaultText, defaultText};
+
+        String blockText = String.valueOf(brightness.block());
+        String skyText = String.valueOf(brightness.sky());
+        return new String[]{blockText, skyText};
     }
 
-    @Override
-    public List<String> complete(@NotNull Player player, String @NotNull [] args, int argIndex) {
-        if (argIndex != 0)
-            return Collections.emptyList();
+    @NotNull
+    private static String backgroundText(@Nullable TextColor background) {
+        if (background == null)
+            return Lang.getString("niveriaholograms.hologram.info.other.default");
 
-        String currentArg = args[argIndex].toLowerCase(Locale.ROOT);
-        return NiveriaHolograms.instance().hologramManager().holograms().stream()
-                .map(Hologram::name)
-                .filter(hologramName -> hologramName.toLowerCase(Locale.ROOT).startsWith(currentArg))
-                .toList();
+        if (background == Hologram.TRANSPARENT)
+            return Lang.getString("niveriaholograms.hologram.info.other.transparent");
+
+        return background.asHexString().toUpperCase(Locale.ROOT);
     }
 }
