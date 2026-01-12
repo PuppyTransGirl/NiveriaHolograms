4bc57ae360e591eab5898162fd324baa2a87a1ed
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-01-12 09:40:42 +0100

    Update HologramEditBillboardCommand

--- src/main/java/toutouchien/niveriaholograms/commands/hologram/edit/general/HologramEditBillboardCommand.java
+++ src/main/java/toutouchien/niveriaholograms/commands/hologram/edit/general/HologramEditBillboardCommand.java
@@ -1,5 +1,6 @@
 package toutouchien.niveriaholograms.commands.hologram.edit.general;
 
+/*
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.minecraft.world.entity.Display;
@@ -90,4 +91,69 @@ public class HologramEditBillboardCommand extends SubCommand {
 				.filter(billboard -> billboard.toLowerCase(Locale.ROOT).startsWith(currentArg))
 				.toList();
 	}
+}*/
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import net.minecraft.world.entity.Display;
+import org.bukkit.command.CommandSender;
+import toutouchien.niveriaapi.lang.Lang;
+import toutouchien.niveriaapi.utils.CommandUtils;
+import toutouchien.niveriaapi.utils.StringUtils;
+import toutouchien.niveriaholograms.NiveriaHolograms;
+import toutouchien.niveriaholograms.core.Hologram;
+import toutouchien.niveriaholograms.managers.HologramManager;
+
+public class HologramEditBillboardCommand {
+    private HologramEditBillboardCommand() {
+        throw new IllegalStateException("Command class");
+    }
+
+    public static LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("billboard")
+                .requires(css -> CommandUtils.defaultRequirements(css, "niveriaholograms.command.hologram.edit.billboard"))
+                .then(Commands.argument("billboard", StringArgumentType.word())
+                        .suggests((ctx, builder) -> {
+                            String hologramName = ctx.getArgument("hologram", String.class);
+
+                            HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
+                            Hologram hologram = hologramManager.hologramByName(hologramName);
+                            Display.BillboardConstraints currentBillboard = hologram == null ? null : hologram.configuration().billboard();
+
+                            for (Display.BillboardConstraints billboard : Display.BillboardConstraints.values())
+                                if (billboard != currentBillboard)
+                                    builder.suggest(billboard.name());
+
+                            return builder.buildFuture();
+                        })
+                        .executes(ctx -> {
+                            CommandSender sender = CommandUtils.sender(ctx);
+                            String hologramName = ctx.getArgument("hologram", String.class);
+                            String billboardName = ctx.getArgument("billboard", String.class);
+
+                            HologramManager hologramManager = NiveriaHolograms.instance().hologramManager();
+                            Hologram hologram = hologramManager.hologramByName(hologramName);
+                            if (hologram == null) {
+                                Lang.sendMessage(sender, "niveriaholograms.hologram.edit.doesnt_exist", hologramName);
+                                return Command.SINGLE_SUCCESS;
+                            }
+
+                            Display.BillboardConstraints billboard = StringUtils.match(billboardName, Display.BillboardConstraints.class, null);
+                            if (billboard == null) {
+                                Lang.sendMessage(sender, "niveriaholograms.hologram.edit.billboard.invalid_billboard", billboardName);
+                                return Command.SINGLE_SUCCESS;
+                            }
+
+                            hologram.editConfig(config -> {
+                                config.billboard(billboard);
+                            });
+
+                            Lang.sendMessage(sender, "niveriaholograms.hologram.edit.billboard.edited");
+                            return Command.SINGLE_SUCCESS;
+                        })
+                ).build();
+    }
 }
