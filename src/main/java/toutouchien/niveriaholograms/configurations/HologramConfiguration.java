4d4a6c430e1a9bf9fa429b0a10915701559e669e
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-01-14 15:02:06 +0100

    Spaces are better than tabs

--- src/main/java/toutouchien/niveriaholograms/configurations/HologramConfiguration.java
+++ src/main/java/toutouchien/niveriaholograms/configurations/HologramConfiguration.java
@@ -5,87 +5,87 @@ import net.minecraft.world.entity.Display;
 import org.joml.Vector3f;
 
 public class HologramConfiguration {
-	private Vector3f scale = new Vector3f(1, 1, 1);
-	private Vector3f translation = new Vector3f(0, 0, 0);
-	private Display.BillboardConstraints billboard = Display.BillboardConstraints.CENTER;
-	private Brightness brightness;
-	private float shadowRadius = 0F;
-	private float shadowStrength = 1F;
-	private int visibilityDistance = -1;
+    private Vector3f scale = new Vector3f(1, 1, 1);
+    private Vector3f translation = new Vector3f(0, 0, 0);
+    private Display.BillboardConstraints billboard = Display.BillboardConstraints.CENTER;
+    private Brightness brightness;
+    private float shadowRadius = 0F;
+    private float shadowStrength = 1F;
+    private int visibilityDistance = -1;
 
-	public Vector3f scale() {
-		return scale;
-	}
+    public Vector3f scale() {
+        return scale;
+    }
 
-	public HologramConfiguration scale(Vector3f scale) {
-		this.scale = scale;
-		return this;
-	}
+    public HologramConfiguration scale(Vector3f scale) {
+        this.scale = scale;
+        return this;
+    }
 
-	public Vector3f translation() {
-		return translation;
-	}
+    public Vector3f translation() {
+        return translation;
+    }
 
-	public HologramConfiguration translation(Vector3f translation) {
-		this.translation = translation;
-		return this;
-	}
+    public HologramConfiguration translation(Vector3f translation) {
+        this.translation = translation;
+        return this;
+    }
 
-	public Display.BillboardConstraints billboard() {
-		return billboard;
-	}
+    public Display.BillboardConstraints billboard() {
+        return billboard;
+    }
 
-	public HologramConfiguration billboard(Display.BillboardConstraints billboard) {
-		this.billboard = billboard;
-		return this;
-	}
+    public HologramConfiguration billboard(Display.BillboardConstraints billboard) {
+        this.billboard = billboard;
+        return this;
+    }
 
-	public Brightness brightness() {
-		return brightness;
-	}
+    public Brightness brightness() {
+        return brightness;
+    }
 
-	public HologramConfiguration brightness(Brightness brightness) {
-		this.brightness = brightness;
-		return this;
-	}
+    public HologramConfiguration brightness(Brightness brightness) {
+        this.brightness = brightness;
+        return this;
+    }
 
-	public float shadowRadius() {
-		return shadowRadius;
-	}
+    public float shadowRadius() {
+        return shadowRadius;
+    }
 
-	public HologramConfiguration shadowRadius(float shadowRadius) {
-		this.shadowRadius = shadowRadius;
-		return this;
-	}
+    public HologramConfiguration shadowRadius(float shadowRadius) {
+        this.shadowRadius = shadowRadius;
+        return this;
+    }
 
-	public float shadowStrength() {
-		return shadowStrength;
-	}
+    public float shadowStrength() {
+        return shadowStrength;
+    }
 
-	public HologramConfiguration shadowStrength(float shadowStrength) {
-		this.shadowStrength = shadowStrength;
-		return this;
-	}
+    public HologramConfiguration shadowStrength(float shadowStrength) {
+        this.shadowStrength = shadowStrength;
+        return this;
+    }
 
-	public int visibilityDistance() {
-		return visibilityDistance;
-	}
+    public int visibilityDistance() {
+        return visibilityDistance;
+    }
 
-	public HologramConfiguration visibilityDistance(int visibilityDistance) {
-		this.visibilityDistance = visibilityDistance;
-		return this;
-	}
+    public HologramConfiguration visibilityDistance(int visibilityDistance) {
+        this.visibilityDistance = visibilityDistance;
+        return this;
+    }
 
-	public HologramConfiguration copy() {
-		HologramConfiguration copy = new HologramConfiguration();
-		copy.scale = new Vector3f(this.scale);
-		copy.translation = new Vector3f(this.translation);
-		copy.billboard = this.billboard;
-		if (this.brightness != null)
-			copy.brightness = new Brightness(this.brightness.block(), this.brightness.sky());
-		copy.shadowRadius = this.shadowRadius;
-		copy.shadowStrength = this.shadowStrength;
-		copy.visibilityDistance = this.visibilityDistance;
-		return copy;
-	}
+    public HologramConfiguration copy() {
+        HologramConfiguration copy = new HologramConfiguration();
+        copy.scale = new Vector3f(this.scale);
+        copy.translation = new Vector3f(this.translation);
+        copy.billboard = this.billboard;
+        if (this.brightness != null)
+            copy.brightness = new Brightness(this.brightness.block(), this.brightness.sky());
+        copy.shadowRadius = this.shadowRadius;
+        copy.shadowStrength = this.shadowStrength;
+        copy.visibilityDistance = this.visibilityDistance;
+        return copy;
+    }
 }
