784026cd0f55ac212032e8894681b3aabaa3f0a4
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-04 16:25:05 +0100

    Optimization & Caching

--- src/main/java/toutouchien/niveriaholograms/updater/HologramUpdater.java
+++ src/main/java/toutouchien/niveriaholograms/updater/HologramUpdater.java
@@ -5,6 +5,8 @@ import net.minecraft.util.Brightness;
 import net.minecraft.world.entity.Display;
 import org.joml.Quaternionf;
 import toutouchien.niveriaholograms.configurations.HologramConfiguration;
+import toutouchien.niveriaholograms.utils.HologramUtils;
+import toutouchien.niveriaholograms.utils.TransformationCtorHolder;
 
 public abstract class HologramUpdater<D extends Display, C extends HologramConfiguration> {
     protected final D display;
@@ -39,41 +41,17 @@ public abstract class HologramUpdater<D extends Display, C extends HologramConfi
             display.setBrightnessOverride(new Brightness(brightness.block(), brightness.sky()));
     }
 
-    @SuppressWarnings({"CastCanBeRemovedNarrowingVariableType", "JavaReflectionInvocation", "JavaReflectionMemberAccess"})
+    @SuppressWarnings("java:S112")
     private void updateTransformation() {
         try {
-            Class<?> vec3fCls = Class.forName("org.joml.Vector3f");
-            Class<?> quatfCls = Class.forName("org.joml.Quaternionf");
-
-            Object t = Transformation.class
-                    .getConstructor(vec3fCls, quatfCls, vec3fCls, quatfCls)
-                    .newInstance(
-                            vec3fCls.cast(config.translation()),
-                            quatfCls.cast(new Quaternionf()),
-                            vec3fCls.cast(config.scale()),
-                            quatfCls.cast(new Quaternionf())
-                    );
-
+            TransformationCtorHolder holder = HologramUtils.transformationCtor();
+            Object t = holder.ctor().newInstance(
+                    holder.vecCls().cast(config.translation()),
+                    holder.quatCls().cast(new Quaternionf()),
+                    holder.vecCls().cast(config.scale()),
+                    holder.quatCls().cast(new Quaternionf())
+            );
             display.setTransformation((Transformation) t);
-        } catch (NoSuchMethodException e) {
-            // fallback to "fc" interfaces
-            try {
-                Class<?> vec3fcCls = Class.forName("org.joml.Vector3fc");
-                Class<?> quatfcCls = Class.forName("org.joml.Quaternionfc");
-
-                Object t = Transformation.class
-                        .getConstructor(vec3fcCls, quatfcCls, vec3fcCls, quatfcCls)
-                        .newInstance(
-                                vec3fcCls.cast(config.translation()),
-                                quatfcCls.cast(new Quaternionf()),
-                                vec3fcCls.cast(config.scale()),
-                                quatfcCls.cast(new Quaternionf())
-                        );
-
-                display.setTransformation((Transformation) t);
-            } catch (Exception ex) {
-                throw new RuntimeException("Could not construct Transformation reflectively", ex);
-            }
         } catch (Exception e) {
             throw new RuntimeException("Could not construct Transformation reflectively", e);
         }
